73a048018e777a62feb0aec548d2fd584fffb275
Jude-njuee <njuegithinjiii@gmail.com>
2025-09-04 01:02:32 +0300

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar, Container, Card } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">My React-Bootstrap App</Navbar.Brand>
+          </Container>
+        </Navbar>
+
+        <Container className="mt-4">
+          <h1 className="text-center">Welcome to My App</h1>
+
+          <div className="d-flex justify-content-around mt-4">
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card 1</Card.Title>
+                <Card.Text>This is the first card.</Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card 2</Card.Title>
+                <Card.Text>This is the second card.</Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card 3</Card.Title>
+                <Card.Text>This is the third card.</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        </Container>
+      </div>
+    </>
   );
 }
 
